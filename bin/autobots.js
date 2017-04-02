d19b863a9c10df2489120da159115c2b4ba8cb5a
David Tudury <david.tudury@gmail.com>
2017-04-02 17:49:24 +0000

    wip

--- bin/autobots.js
+++ bin/autobots.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/data/data/com.termux/files/usr/bin/env node
 
 "use strict";
 
@@ -8,8 +8,18 @@ const path = require("path");
 const lib = require("../lib");
 
 let cwd = process.cwd();
+let mode = process.argv.slice(2).join(' ');
+let autobots;
 
-const load_config = () => lib.fs.readFile(path.join(cwd, ".autobots.json"), "utf8")
+console.log(mode);
+console.log(mode);
+console.log(mode);
+//go == transform and rollout (?)
+//rollout
+//transform
+
+//side-effect: fixes cwd to .json location
+let load_config = () => lib.fs.readFile(path.join(cwd, ".autobots.json"), "utf8")
     .catch(err => {
         let parent = path.dirname(cwd);
         if (err.code === "ENOENT" && parent !== cwd) {
@@ -19,27 +29,50 @@ const load_config = () => lib.fs.readFile(path.join(cwd, ".autobots.json"), "utf
         console.error(err);
         throw new Error(`unhandled error code: ${err.code} in load_config`);
     })
+    
+let expand_paths = () => {
+    autobots.forEach(autobot => {
+        autobot.src = path.join(cwd, autobot.src);
+        autobot.dest = path.join(cwd, autobot.dest);
+    })
+    return autobots;
+};
+
+let fix_labels = () => {
+    let longest_label = 0;
+    autobots.forEach(autobot => {
+        autobot.label = autobot.label || `${autobot.dest} (${autobot.transformer})`;
+        longest_label = Math.max(longest_label, autobot.label.length)
+    })
+    autobots.forEach(autobot => {
+        while (autobot.label.length < longest_label) autobot.label += " ";
+    })
+    return autobots;
+};
+
+let fork_autobots = () => {
+    autobots.forEach(autobot => {
+        child_process.fork(path.join(__dirname, "../lib/child.js"), process.argv.slice(2), {cwd}).send(autobot);
+    });
+    return autobots;
+};
+
 
 load_config()
-    .then(JSON.parse)
+    .then(config => autobots = JSON.parse(config))
+    .then(fix_labels)
+    .then(expand_paths)
+/*
     .then(autobots => {
-        let longest_label = 0;
-        autobots.forEach(autobot => {
-            autobot.src = path.join(cwd, autobot.src);
-            autobot.dest = path.join(cwd, autobot.dest);
-            autobot.label = autobot.label || `${autobot.dest} (${autobot.transformer})`;
-            longest_label = Math.max(longest_label, autobot.label.length)
-        })
-        Promise.all(autobots.map(autobot => lib.fs.mkdirp(path.dirname(autobot.dest))))
-        .then(() => {
-            autobots.forEach(autobot => {
-                while (autobot.label.length < longest_label) autobot.label += " ";
-                let child = child_process.fork(path.join(__dirname, "../lib/child.js"), process.argv.slice(2), {cwd});
-                child.send(autobot);
-            });
-        })
+    	    switch(mode) {
+    	        case "go":
+    	            return mkdirps(autobots)
+    	    }
     })
+*/
+    .then(() => Promise.all(autobots.map(autobot => lib.fs.mkdirp(path.dirname(autobot.dest)))))
+    .then(fork_autobots)
     .catch(err => {
         console.error(err);
-        throw new Error("error loading config");
+        throw new Error(`error running autobots ${mode}`);
     });
